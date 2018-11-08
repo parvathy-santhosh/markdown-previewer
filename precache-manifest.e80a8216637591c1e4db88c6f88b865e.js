89ed427fe0ee259a9b00a4cfc88b783b0cd727d2
Parvathy Santhosh <parsan1998@gmail.com>
2018-11-08 21:28:34 +0530

    Updates

---
+++ precache-manifest.e80a8216637591c1e4db88c6f88b865e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c248355f6dd8174963fc",
+    "url": "/static/js/main.c248355f.chunk.js"
+  },
+  {
+    "revision": "34987c8747ee8ac6ddbc",
+    "url": "/static/js/1.34987c87.chunk.js"
+  },
+  {
+    "revision": "c248355f6dd8174963fc",
+    "url": "/static/css/main.b85a7022.chunk.css"
+  },
+  {
+    "revision": "365f8eae3b443b4dde6263a2d1280943",
+    "url": "/index.html"
+  }
+];
