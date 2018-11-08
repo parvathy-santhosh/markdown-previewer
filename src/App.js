267b0bb723c740d217be7f18858cbd21957fe735
Parvathy Santhosh <parsan1998@gmail.com>
2018-11-08 22:42:52 +0530

    Added PlaceHolder

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import MarkDown from './MarkDown.js';
 
+const PLACEHOLDER = '# Heading 1 \n\n   ## Heading 2 \n [link here](https://www.freecodecamp.com) \n  \n`inline code` \n<pre>Preformatted text</pre> \n ``` \n code block \n ``` \n \n **bolded text** \n * List item \n >Block quotes \n\n ![alt text](https://github.com/parvathy-santhosh/markdown-previewer/raw/master/screenshot.PNG?raw=true)';
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text : ''
+      text : PLACEHOLDER
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -22,7 +24,7 @@ class App extends Component {
         <h1> MarkDown Previewer </h1>
         <hr className="hr margin" />
         <div>
-          <textarea id="editor" className="a-block" onChange={this.handleChange} />
+          <textarea id="editor" className="a-block" onChange={this.handleChange} placeholder={PLACEHOLDER}/>
           <MarkDown text={this.state.text}/>
         </div>
         <hr className="hr margin" />
